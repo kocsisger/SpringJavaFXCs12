1b6a1bff98c97fb5113bbbe2ac7bcad09288e288
Teszt Elek <you@inf.unideb.hu>
2024-10-31 13:20:40 +0100

    CommandLineRunner

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -1,22 +1,20 @@
 package hu.unideb.inf.SpringJavaFX;
 
 import hu.unideb.inf.SpringJavaFX.model.Person;
+import hu.unideb.inf.SpringJavaFX.model.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class SpringJavaFxApplication {
+public class SpringJavaFxApplication implements CommandLineRunner {
+
+	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
-
-		/*Person p = new Person();
-		p.setId(10);
-		p.setName("Robert");
-		p.setDateOfBirth(LocalDate.of(1998,12,15));
-		Person p2 = new Person(1,"tom", LocalDate.of(1999,11,11));*/
 
 		Person p = Person.builder()
 				.name("Winch Eszter")
@@ -24,4 +22,8 @@ public class SpringJavaFxApplication {
 				.build();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
