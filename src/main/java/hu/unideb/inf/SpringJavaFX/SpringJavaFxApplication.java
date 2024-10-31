02afb0f2f445de14b3bf8c274ef27dc5f674c8fc
Teszt Elek <you@inf.unideb.hu>
2024-10-31 12:43:35 +0100

    Person pojo, lombok

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -1,13 +1,22 @@
 package hu.unideb.inf.SpringJavaFX;
 
+import hu.unideb.inf.SpringJavaFX.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class SpringJavaFxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
+
+		Person p = new Person();
+		p.setId(10);
+		p.setName("Robert");
+		p.setDateOfBirth(LocalDate.of(1998,12,15));
+		Person p2 = new Person(1,"tom", LocalDate.of(1999,11,11));
 	}
 
 }
