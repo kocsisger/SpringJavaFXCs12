2d90848db347910b1fdfffbff07148a046ba3a18
Teszt Elek <you@inf.unideb.hu>
2024-10-31 13:26:23 +0100

    CommandLineRunner + Repository

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -2,6 +2,7 @@ package hu.unideb.inf.SpringJavaFX;
 
 import hu.unideb.inf.SpringJavaFX.model.Person;
 import hu.unideb.inf.SpringJavaFX.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,19 +12,21 @@ import java.time.LocalDate;
 @SpringBootApplication
 public class SpringJavaFxApplication implements CommandLineRunner {
 
+	@Autowired
 	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
-
-		Person p = Person.builder()
-				.name("Winch Eszter")
-				.dateOfBirth(LocalDate.of(1999,11,05))
-				.build();
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Person p = Person.builder()
+				.name("Winch Eszter")
+				.dateOfBirth(LocalDate.of(1999,11,05))
+				.build();
+		System.out.println(p);
+		personRepository.save(p);
+		System.out.println("db -> " + personRepository.findByName("Winch Eszter"));
 	}
 }
