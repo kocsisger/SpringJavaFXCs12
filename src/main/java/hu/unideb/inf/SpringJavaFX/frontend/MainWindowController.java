51d64c008daa5185c0675e6e557ea2db99da9b97
Teszt Elek <you@inf.unideb.hu>
2024-11-07 13:29:06 +0100

    Final final

--- src/main/java/hu/unideb/inf/SpringJavaFX/frontend/MainWindowController.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/frontend/MainWindowController.java
@@ -1,4 +1,15 @@
 package hu.unideb.inf.SpringJavaFX.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainWindowController {
+    @FXML
+    public Label nameLabel;
+
+    @FXML
+    public void loadData(ActionEvent actionEvent) {
+        nameLabel.setText(JavaFXMain.manager.getPersonName());
+    }
 }
